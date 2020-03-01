ddfb1de72a5d6030dffba7c521a4e172430ad3ed
pvtyuan <pvtyuan@foxmail.com>
2020-03-02 01:32:35 +0800

    fix bug when concurrent connection

--- src/main/java/io/rapidw/mqtt/client/MqttClient.java
+++ src/main/java/io/rapidw/mqtt/client/MqttClient.java
@@ -6,24 +6,27 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.concurrent.DefaultThreadFactory;
+import lombok.val;
 
 public class MqttClient {
-    private Bootstrap bootstrap;
+    private EventLoopGroup eventLoopGroup;
 
     public MqttClient() {
-         EventLoopGroup eventLoopGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2,
+         eventLoopGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2,
             new DefaultThreadFactory(MqttClientConstants.ThreadNamePrefix, true));
-        this.bootstrap = new Bootstrap().group(eventLoopGroup).channel(NioSocketChannel.class)
-            .option(ChannelOption.SO_REUSEADDR, true)
-            .option(ChannelOption.TCP_NODELAY, true);
+
     }
 
     public MqttConnection newConnection(MqttConnectionOption connectionOption) {
+        val bootstrap = new Bootstrap().group(eventLoopGroup).channel(NioSocketChannel.class)
+            .option(ChannelOption.SO_REUSEADDR, true)
+            .option(ChannelOption.TCP_NODELAY, true);
         if (connectionOption.getTcpConnectTimeout() != 0) {
             bootstrap.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionOption.getTcpConnectTimeout());
         }
         MqttConnection connection = new MqttConnection(bootstrap, connectionOption);
-        this.bootstrap.handler(new MqttChannelInitializer(connectionOption, connection.handler()));
+
+        bootstrap.handler(new MqttChannelInitializer(connectionOption, connection.handler()));
         return connection;
     }
 }
